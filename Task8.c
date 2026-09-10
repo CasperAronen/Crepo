52a29a9d7dafb1a83cd8d99885497dbcd8975a83
CasperAronen <casperaronen@gmail.com>
2026-09-10 23:05:53 +0300

    Completed Task8

--- Task8.c
+++ Task8.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int printNumbers(const int*array, int count) {
+    for (int i = 0; i < count; i++) {
+        printf("%d ", array[i]);
+    }
+}
+
+int main() {
+    int numbers[15];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 15; i++) {
+        numbers[i] = rand() % 100;
+    }
+    printNumbers(numbers, 15);
+    return 0;
+}
